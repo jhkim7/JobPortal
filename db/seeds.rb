b6a049327c3e5bb6ee2f8ae2effb361e9640d41c
richard <rgschaef@ncsu.edu>
2014-09-26 17:05:12 -0400

    Sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,14 @@ Job.create(title: 'Taxi Driver', description:'Drive car, defend self from hobos'
 Job.create(title: 'Bartender', description:'Serve drinks, defend self from hobos', tag:'alcohol, bums', created_at:'', updated_at:'')
 Job.create(title: 'Line cook', description:'Flip burgers', tag:'food, burgers', created_at:'', updated_at:'')
 
+JobSeeker.create(name: 'John Smith', email: 'johnsmith@gmail.com', password: 'password', phone_number: '5556781234', skills: 'computers, development', resume: 'sample plaintext resume here' )
+JobSeeker.create(name: 'Generic Programmer', email: 'genericprogrammer@gmail.com', password: 'password', phone_number: '5556781232', skills: 'computers, development', resume: 'sample plaintext resume here' )
+JobSeeker.create(name: 'Generic Programmer II', email: 'genericprogrammerii@gmail.com', password: 'password', phone_number: '5556781231', skills: 'computers, development', resume: 'sample plaintext resume here' )
+JobSeeker.create(name: 'Danny Gobel', email: 'dannygobel@gmail.com', password: 'password', phone_number: '5556781235', skills: 'developer, stuff', resume: 'sample plaintext resume here' )
+JobSeeker.create(name: 'Ethan Generic', email: 'ethangeneric@gmail.com', password: 'password', phone_number: '5556781237', skills: 'food, driving', resume: 'sample plaintext resume here' )
+JobSeeker.create(name: 'Hobo Defender', email: 'hobodefender@gmail.com', password: 'password', phone_number: '5556781239', skills: 'defend self from hobos', resume: 'Name says it all.' )
 
-
+JobApplication.create(job: 'Web Developer', job_seeker: 'John Smith', cover_letter: 'plaintext cover letter filler', application_date: '', status:'Pending')
+JobApplication.create(job: 'Web Developer', job_seeker: 'Generic Programmer', cover_letter: 'plaintext cover letter filler', application_date: '', status:'Not a Fit')
+JobApplication.create(job: 'Web Developer', job_seeker: 'Generic Programmer II', cover_letter: 'plaintext cover letter filler', application_date: '', status:'Future Interview')
 
