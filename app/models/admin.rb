940692e12af627eeb94ee801ebfc6fc8075d6fbf
hrcerqueira@gmail.com <jhkim7@ncsu.edu>
2014-09-26 18:40:10 -0400

    Login session working

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -6,25 +6,25 @@ class Admin < ActiveRecord::Base
   validates :password, :confirmation => true #password_confirmation attr
   validates_length_of :password, :in => 6..20, :on => :create
   
-  # before_save :encrypt_password
+  before_save :encrypt_password
   after_save :clear_password
 
-  # def encrypt_password
-    # if password.present?
-      # self.salt = BCrypt::Engine.generate_salt
-      # self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
-    # end
-  # end
-#   
+  def encrypt_password
+    if password.present?
+      self.salt = BCrypt::Engine.generate_salt
+      self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
+    end
+  end
+
   def clear_password
     self.password = nil
   end
   
   def self.authenticate(name_or_email="", login_password="")
     if  EMAIL_REGEX.match(name_or_email)    
-      user = User.find_by_email(name_or_email)
+      user = Admin.find_by_email(name_or_email)
     else
-      user = User.find_by_name(name_or_email)
+      user = Admin.find_by_name(name_or_email)
     end
     if user && user.match_password(login_password)
       return user
--- config/routes.rb
+++ config/routes.rb
@@ -10,18 +10,21 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
+  get 'sessions/logout'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-    resources :jobs  
-
-  resources :employers  
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
- 
   resources :admins
-  
+  resources :sessions
+  resources :jobs
+  resources :employers
 
+  # You can have the root of your site routed with "root"
+  root 'sessions#login'
+
+  get 'login' => 'sessions#login'
+  post 'login' => 'sessions#login'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
